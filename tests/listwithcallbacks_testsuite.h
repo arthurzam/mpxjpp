d700c9d6d36bc86f08757ba251d7a811e2aeaec3
Zamarin Arthur <arthurzam@gmail.com>
2018-01-26 13:23:43 +0200

    Add base for testing

---
+++ tests/listwithcallbacks_testsuite.h
@@ -0,0 +1,62 @@
+#ifndef LISTWITHCALLBACKS_TESTSUITE_H
+#define LISTWITHCALLBACKS_TESTSUITE_H
+
+#include <cxxtest/TestSuite.h>
+
+#include <mpxjpp/listwithcallbacks.h>
+
+class ListWithCallbacks_Test : public CxxTest::TestSuite {
+private:
+	class SubListTest : public mpxjpp::ListWithCallbacks<int> {
+		protected:
+			virtual void added(int index) override {
+				throw int(at(index));
+			}
+			virtual void removed(const int &num) override {
+				throw int(num);
+			}
+			virtual void replaced(const int &older, const int &newer) override {
+				throw int(newer * 0x10000 + older);
+			}
+	};
+public:
+	void testAddSignal() {
+		SubListTest s;
+		TS_ASSERT_THROWS_EQUALS(s.add(2), int n, n, 2);
+		TS_ASSERT_EQUALS(s.size(), 1);
+		TS_ASSERT_THROWS_EQUALS(s.add(3), int n, n, 3);
+		TS_ASSERT_THROWS_EQUALS(s.add(5), int n, n, 5);
+		TS_ASSERT_EQUALS(s.size(), 3);
+
+		TS_ASSERT_THROWS_EQUALS(s.add(4, 1), int n, n, 4);
+		TS_ASSERT_EQUALS(s.at(1), 4);
+		TS_ASSERT_EQUALS(s.at(2), 3);
+	}
+
+	void testRemoveSignal() {
+		SubListTest s;
+		TS_ASSERT_THROWS_ANYTHING(s.add(2));
+		TS_ASSERT_THROWS_ANYTHING(s.add(3));
+		TS_ASSERT_THROWS_ANYTHING(s.add(5));
+		TS_ASSERT_EQUALS(s.size(), 3);
+
+		TS_ASSERT_THROWS_EQUALS(s.remove(1), int n, n, 3);
+		TS_ASSERT_EQUALS(s.size(), 2);
+		TS_ASSERT_THROWS_EQUALS(s.remove(1), int n, n, 5);
+		TS_ASSERT_EQUALS(s.size(), 1);
+		TS_ASSERT_THROWS_EQUALS(s.remove(0), int n, n, 2);
+		TS_ASSERT(s.empty());
+	}
+
+	void testReplaceSignal() {
+		SubListTest s;
+		TS_ASSERT_THROWS_ANYTHING(s.add(2));
+		TS_ASSERT_THROWS_ANYTHING(s.add(3));
+		TS_ASSERT_THROWS_ANYTHING(s.add(5));
+		TS_ASSERT_EQUALS(s.size(), 3);
+
+		TS_ASSERT_THROWS_EQUALS(s.set(1, 6), int n, n, 6 * 0x10000 + 3);
+		TS_ASSERT_EQUALS(s.at(1), 6);
+	}
+};
+#endif // LISTWITHCALLBACKS_TESTSUITE_H
---
+++ tests/tests.pro
@@ -0,0 +1,27 @@
+CONFIG += c++14
+
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+
+QMAKE_CLEAN += $(TARGET)
+
+INCLUDEPATH += $$PWD/../
+
+#LIBS += -L../src -lmylib
+
+HEADERS += \
+    object_testsuite.h \
+    listwithcallbacks_testsuite.h
+
+TEST_HEADERS = $$find(HEADERS, .*testsuite.h)
+
+generate_tests.name = "Generate CxxTests"
+generate_tests.input = TEST_HEADERS
+generate_tests.output =  $$PWD/runner.cc
+generate_tests.commands = cxxtestgen --root --have-std --have-eh --error-printer -o ${QMAKE_FILE_OUT} ${QMAKE_FILE_IN}
+generate_tests.CONFIG += no_link target_predeps combine
+generate_tests.clean += ${QMAKE_FILE_OUT}
+generate_tests.variable_out = GENERATED_SOURCES
+
+QMAKE_EXTRA_COMPILERS += generate_tests
